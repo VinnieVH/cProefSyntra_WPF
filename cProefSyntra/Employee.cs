99ef230f8d47947b0a0b00134e4b2761f3494c24
VincentVH <vincentvanherreweghe@gmail.com>
2018-08-22 16:25:16 +0200

    Add basic database logic

---
+++ cProefSyntra/Employee.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cProefSyntra
+{
+    class Employee
+    {
+        public ObjectId Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public Employee(string first_Name, string last_Name)
+        {
+            FirstName = first_Name;
+            LastName = last_Name;
+        }
+
+    }
+}
--- cProefSyntra/LoginWindow.xaml.cs
+++ cProefSyntra/LoginWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using MahApps.Metro.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace cProefSyntra
 {
@@ -23,6 +15,45 @@ namespace cProefSyntra
         public LoginWindow()
         {
             InitializeComponent();
+        }
+
+        MainWindow main = new MainWindow();
+        RegisterWindow registration = new RegisterWindow();
+        MongoClient client = new MongoClient("mongodb://VincentVH:Vincent159Derp@ds217560.mlab.com:17560/c-proef_syntra");
+
+
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(TxtBoxEmail.Text.Length == 0)
+            {
+                EmailErrorMessage.Text = "Enter an email.";
+                TxtBoxEmail.Focus();
+            }
+            else if(!Regex.IsMatch(TxtBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                EmailErrorMessage.Text = "Enter a valid email.";
+                TxtBoxEmail.Select(0, TxtBoxEmail.Text.Length);
+                TxtBoxEmail.Focus();
+            }
+            else
+            {
+                string email = TxtBoxEmail.Text;
+                string password = PwBoxPassword.Password;
+
+
+                /// Testing DB code (needs to be replaced)
+                var db = client.GetDatabase("c-proef_syntra");
+                var collection = db.GetCollection<Employee>("employees");
+                Employee employee = new Employee("Vincent", "Van Herreweghe");
+                collection.InsertOne(employee);
+            }
+        }
+
+        private void RegisterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            registration.Show();
+            Close();
         }
     }
 }
